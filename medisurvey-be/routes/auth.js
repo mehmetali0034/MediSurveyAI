5c701f8427b52f3e9faf88b909de5fe4695dfa6f
Ali Kolcuk <mehmetalikolcuk@gmail.com>
2025-01-06 17:48:39 +0300

    fix

--- medisurvey-be/routes/auth.js
+++ medisurvey-be/routes/auth.js
@@ -1,8 +1,6 @@
 const express = require('express');
-<<<<<<< HEAD
+
 const { register, login } = require('../controllers/authController.js');
-=======
->>>>>>> 0b0364082c3f02f3f1fd2a7530f40098ecda3f63
 const router = express.Router();
 const { 
   adminRegisterDoctor, 
@@ -22,13 +20,7 @@ router.post('/login-doctor', loginDoctor);
 
 router.post('/tenant/register', registerTenant);
 
-<<<<<<< HEAD
-// Kullanıcı kaydı ve girişi rotalarını tanımlar
-router.post('/register', register);
-router.post('/login', login);
-module.exports = router;
-=======
 router.post('/tenant/login', loginTenant);
 
 module.exports = router;
->>>>>>> 0b0364082c3f02f3f1fd2a7530f40098ecda3f63
+
