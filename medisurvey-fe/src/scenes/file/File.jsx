fc0a4681c1db6defa108b7c5af3725624774339f
Ali Kolcuk <mehmetalikolcuk@gmail.com>
2025-04-20 14:39:58 +0300

    fixed

--- medisurvey-fe/src/scenes/file/File.jsx
+++ medisurvey-fe/src/scenes/file/File.jsx
@@ -27,14 +27,7 @@ export default function File() {
     };
     fetchFileInfo();
   }, []);
-  const clickToForDoctor =()=>{
-    setForPatient("0")
-    console.log(forPatient)
-  }
-  const clickToForPatient =()=>{
-    setForPatient("1")
-    console.log(forPatient)
-  }
+  
   return (
     <Box>
       <Box
@@ -47,16 +40,9 @@ export default function File() {
       >
         <Headeer
           title="File Detail"
-          subtitle={`Add Form at ${fileInfo.name}`}
+          subtitle={`Display File Detail`}
         />
-        <Box sx={{alignItems:"center"}}>
-          <Button sx={{backgroundColor:colors.blueAccent[400],mr:2}} onClick={clickToForDoctor} variant="contained">
-            Add Form to Doctor
-          </Button>
-          <Button  sx={{backgroundColor:colors.blueAccent[400],mr:2}} onClick={clickToForPatient}  variant="contained">
-            Add Form to Patient
-          </Button>
-        </Box>
+       
       </Box>
     </Box>
   );
